d5b68eaec5be7d645b7fec36a2e700d44cec58b9
hanocha <saiki@feedforce.jp>
2018-02-13 23:07:05 +0900

    Remove old recipes

--- cookbooks/oh-my-zsh/default.rb
+++
@@ -1,5 +0,0 @@
-execute 'install oh-my-zsh' do
-  user node.user.name
-  cwd "/home/#{node.user.name}"
-  command %|sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"|
-end
--- cookbooks/zsh/default.rb
+++
@@ -1,5 +0,0 @@
-package 'zsh'
-
-execute 'change default shell to zsh' do
-  command "chsh -s /bin/zsh #{node.user.name}"
-end
