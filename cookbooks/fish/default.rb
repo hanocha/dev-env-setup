3302b41a6ec8e7f1dec130ee98ee0968d50ba418
hanocha <saiki@feedforce.jp>
2018-02-13 23:06:47 +0900

    Add some codes for setting fish

--- cookbooks/fish/default.rb
+++ cookbooks/fish/default.rb
@@ -6,3 +6,18 @@ EOS
 end
 
 package "fish"
+
+execute "set default shell to fish" do
+  command "chsh -s /usr/bin/fish #{node.user.name}"
+end
+
+execute "Install fisherman" do
+  user node.user.name
+  command "curl -Lo /home/#{node.user.name}/.config/fish/functions/fisher.fish --create-dirs https://git.io/fisher"
+end
+
+execute "Install some plugins" do
+  user node.user.name
+  cwd "/home/#{node.user.name}"
+  command "fish -c 'fisher oh-my-fish/theme-bobthefish'"
+end
