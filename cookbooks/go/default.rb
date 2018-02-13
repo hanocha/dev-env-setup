9fab4d6d2373d5d1bf76e798d7621d7b6699c319
hanocha <saiki@feedforce.jp>
2018-02-14 01:11:04 +0900

    Edit go cookbook, use latest go binary and use node info

--- cookbooks/go/default.rb
+++ cookbooks/go/default.rb
@@ -1,20 +1,23 @@
-execute 'download go 1.7.5 binaly' do
+home_dir = "/home/#{node.user.name}"
+
+execute 'download go 1.9.4 binaly' do
   user node.user.name
-  cwd '/home/hanocha'
-  command 'wget https://storage.googleapis.com/golang/go1.7.5.linux-amd64.tar.gz'
-  not_if 'ls /home/hanocha/go1.7.5.linux-amd64.tar.gz'
+  cwd home_dir
+  command 'wget https://storage.googleapis.com/golang/go1.9.4.linux-amd64.tar.gz'
+  not_if "ls #{home_dir}/go1.9.4.linux-amd64.tar.gz"
 end
 
 execute 'decompless go binaly tarball' do
-  command 'tar -C /usr/local -xzf /home/hanocha/go1.7.5.linux-amd64.tar.gz'
+  command "tar -C /usr/local -xzf #{home_dir}/go1.9.4.linux-amd64.tar.gz"
   not_if 'ls /usr/local/go'
 end
 
 execute 'add go pathes into .zshrc' do
   user node.user.name
-  command %|echo 'export GOROOT="/usr/local/go"' >> /home/hanocha/.zshrc
-echo 'export GOPATH="/home/hanocha/go"' >> /home/hanocha/.zshrc
-echo 'export PATH=$PATH:$GOROOT/bin:$GOPATH/bin' >> /home/hanocha/.zshrc
+  command %|
+  echo 'set -x GOROOT "/usr/local/go"' >> /home/#{node.user.name}/.config/fish/config.fish
+  echo 'set -x GOPATH "/home/hanocha/go"' >> /home/#{node.user.name}/.config/fish/config.fish
+  echo 'set -x PATH $GOROOT/bin $GOPATH/bin $PATH' >> /home/#{node.user.name}/.config/fish/config.fish
 |
-  not_if 'grep "go/bin" /home/hanocha/.zshrc'
+  not_if "cat #{home_dir}/.config/fish/config.fish | grep 'set -x PATH $GOROOT/bin $GOPATH/bin $PATH'"
 end
