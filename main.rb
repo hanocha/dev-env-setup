9c9e11966a9ca9514229793883b5a8479255f839
hanocha <saiki@feedforce.jp>
2018-02-14 01:15:39 +0900

    Update main entry point file

--- main.rb
+++ main.rb
@@ -1,13 +1,8 @@
-Specinfra::Command::Linuxmint = Class.new
-Specinfra::Command::Linuxmint::Base = Class.new(Specinfra::Command::Debian::Base)
-Specinfra::Command::Linuxmint::Base::Ppa = Class.new(Specinfra::Command::Debian::Base::Ppa)
-Specinfra::Command::Linuxmint::Base::Service = Class.new(Specinfra::Command::Debian::Base::Service)
-
-include_recipe "./cookbooks/vim/default.rb"
-include_recipe "./cookbooks/zsh/default.rb"
-include_recipe "./cookbooks/oh-my-zsh/default.rb"
-include_recipe "./cookbooks/tmux/default.rb"
-include_recipe "./cookbooks/ruby/default.rb"
+include_recipe "./cookbooks/curl/default.rb"
+include_recipe "./cookbooks/powerline-font/default.rb"
+include_recipe "./cookbooks/fish/default.rb"
 include_recipe './cookbooks/go/default.rb'
-include_recipe './cookbooks/ghq/default.rb'
-include_recipe './cookbooks/peco/default.rb'
+# include_recipe './cookbooks/ghq/default.rb'
+# include_recipe './cookbooks/peco/default.rb'
+# include_recipe "./cookbooks/vim/default.rb"
+# include_recipe "./cookbooks/ruby/default.rb"
