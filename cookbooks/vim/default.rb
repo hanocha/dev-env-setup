068bbdbdc0e5ab9e9ffcf23d6e2cda4fbaf0e0e5
hanocha <saiki@feedforce.jp>
2018-02-14 19:42:20 +0900

    Add a recipe to install and set default editor vim

--- cookbooks/vim/default.rb
+++ cookbooks/vim/default.rb
@@ -1,1 +1,6 @@
-package "vim-gnome"
+package "neovim"
+
+execute 'set default editor to vim' do
+  command %|echo 'set -x EDITOR vim' >> /home/#{node.user.name}/.config/fish/config.fish|
+  not_if "grep 'set -x EDITOR' /home/#{node.user.name}/.config/fish/config.fish"
+end
