e7abccdff56688833adccb79232865d481aa9a63
hanocha <hanocha.peter@gmail.com>
2018-06-10 20:31:32 +0900

    Run recipe of docker

--- main.rb
+++ main.rb
@@ -1,9 +1,10 @@
-include_recipe "./cookbooks/curl/default.rb"
-include_recipe "./cookbooks/powerline-font/default.rb"
-include_recipe "./cookbooks/fish/default.rb"
+include_recipe './cookbooks/curl/default.rb'
+include_recipe './cookbooks/powerline-font/default.rb'
+include_recipe './cookbooks/fish/default.rb'
 include_recipe './cookbooks/go/default.rb'
 include_recipe './cookbooks/ghq/default.rb'
 include_recipe './cookbooks/peco/default.rb'
-include_recipe "./cookbooks/vim/default.rb"
-# include_recipe "./cookbooks/anyenv/default.rb"
-include_recipe "./cookbooks/ruby/default.rb"
+include_recipe './cookbooks/vim/default.rb'
+# include_recipe './cookbooks/anyenv/default.rb'
+include_recipe './cookbooks/ruby/default.rb'
+include_recipe './cookbooks/docker/default.rb'
