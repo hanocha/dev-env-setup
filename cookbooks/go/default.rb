20f8bdef62ef6524f6d188bc1300341390bc5897
hanocha <tadasofu@gmail.com>
2019-06-13 20:22:22 +0900

    Update go version

--- cookbooks/go/default.rb
+++ cookbooks/go/default.rb
@@ -1,14 +1,14 @@
 home_dir = "/home/#{node.user.name}"
 
-execute 'download go 1.9.4 binaly' do
+execute 'download go 1.12.6 binaly' do
   user node.user.name
   cwd home_dir
-  command 'wget https://storage.googleapis.com/golang/go1.9.4.linux-amd64.tar.gz'
-  not_if "ls #{home_dir}/go1.9.4.linux-amd64.tar.gz"
+  command 'wget https://dl.google.com/go/go1.12.6.linux-amd64.tar.gz'
+  not_if "ls #{home_dir}/go1.12.6.linux-amd64.tar.gz"
 end
 
 execute 'decompless go binaly tarball' do
-  command "tar -C /usr/local -xzf #{home_dir}/go1.9.4.linux-amd64.tar.gz"
+  command "tar -C /usr/local -xzf #{home_dir}/go1.12.6.linux-amd64.tar.gz"
   not_if 'ls /usr/local/go'
 end
 
