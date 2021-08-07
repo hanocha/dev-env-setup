5dfcf97b146e6e294844f0b8a53acd98eb74dafd
hanocha <tadasofu@gmail.com>
2021-08-07 21:27:51 +0900

    Use package command to install peco

--- cookbooks/peco/default.rb
+++ cookbooks/peco/default.rb
@@ -1,8 +1,4 @@
-execute 'go get peco' do
-  user node.user.name
-  command "env GOPATH='/home/#{node.user.name}/go' /usr/local/go/bin/go get github.com/peco/peco/cmd/peco"
-  not_if "ls /home/#{node.user.name}/go/bin/peco"
-end
+package 'peco'
 
 execute 'set alias' do
   command %[echo 'alias gmv="cd (ghq root)/(ghq list | peco)"' >> /home/#{node.user.name}/.config/fish/config.fish]
