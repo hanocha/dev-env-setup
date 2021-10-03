e16671cf80751063a1bc4b530f92aef03fbed229
hanocha <shumpei.s@hanolab.net>
2021-10-03 12:14:56 +0900

    Update go cookbook

--- cookbooks/go/default.rb
+++ cookbooks/go/default.rb
@@ -1,23 +1,22 @@
 home_dir = "/home/#{node.user.name}"
 
-execute 'download go 1.16 binary' do
+execute 'download go 1.17 binary' do
   user node.user.name
   cwd home_dir
-  command 'wget https://golang.org/dl/go1.16.linux-amd64.tar.gz'
-  not_if "ls #{home_dir}/go1.16.linux-amd64.tar.gz"
+  command 'wget https://golang.org/dl/go1.17.1.linux-amd64.tar.gz -O go.tar.gz'
+  not_if "ls #{home_dir}/go.tar.gz"
 end
 
 execute 'decompless go binary tarball' do
-  command "tar -C /usr/local -xzf #{home_dir}/go1.16.linux-amd64.tar.gz"
-  not_if 'ls /usr/local/go'
+  command "rm -rf /usr/local/go && tar -C /usr/local -xzf #{home_dir}/go.tar.gz"
 end
 
-execute 'add go pathes into .zshrc' do
+execute 'add go pathes into fish config' do
   user node.user.name
-  command %|
-  echo 'set -x GOROOT /usr/local/go' >> /home/#{node.user.name}/.config/fish/config.fish
-  echo 'set -x GOPATH $HOME/go' >> /home/#{node.user.name}/.config/fish/config.fish
-  echo 'set -x PATH $GOROOT/bin $GOPATH/bin $PATH' >> /home/#{node.user.name}/.config/fish/config.fish
-|
-  not_if "cat #{home_dir}/.config/fish/config.fish | grep 'set -x PATH $GOROOT/bin $GOPATH/bin $PATH'"
+  command "echo 'set -x PATH /usr/local/go/bin $PATH' >> /home/#{node.user.name}/.config/fish/config.fish"
+  not_if "cat #{home_dir}/.config/fish/config.fish | grep 'set -x PATH /usr/local/go/bin $PATH'"
+end
+
+file "#{home_dir}/go.tar.gz" do
+  action :delete
 end
