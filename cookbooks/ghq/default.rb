113a910c3426b8c1813bf8ffbf02fb5560723e67
hanocha <tadasofu@gmail.com>
2021-10-11 01:17:24 +0900

    Modify repo name of ghq

--- cookbooks/ghq/default.rb
+++ cookbooks/ghq/default.rb
@@ -1,5 +1,5 @@
 execute 'go get ghq' do
   user node.user.name
-  command "env GOPATH='/home/#{node.user.name}/go' /usr/local/go/bin/go get github.com/motemen/ghq"
+  command "/usr/local/go/bin/go get github.com/x-motemen/ghq"
   not_if "ls /home/#{node.user.name}/go/bin/ghq"
 end
