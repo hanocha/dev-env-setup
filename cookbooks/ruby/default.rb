d6de2f267342ddb76b85da29a3df51c905345a80
hanocha <shumpei.s@hanolab.net>
2022-05-05 19:19:42 +0900

    Install rbenv by git

--- cookbooks/ruby/default.rb
+++ cookbooks/ruby/default.rb
@@ -1,1 +1,23 @@
-package "rbenv"
+git "rbenv" do
+  user node.user.name
+  repository "https://github.com/rbenv/rbenv.git"
+  destination "/home/#{node.user.name}/.rbenv"
+end
+
+execute 'Add rbenv settings into config.fish' do
+  user node.user.name
+  command <<~COMMAND
+    echo 'set -Ux fish_user_paths $HOME/.rbenv/bin $fish_user_paths' >> /home/#{node.user.name}/.config/fish/config.fish
+    echo 'status --is-interactive; and rbenv init - fish | source' >> /home/#{node.user.name}/.config/fish/config.fish
+  COMMAND
+  not_if "cat /home/#{node.user.name}/.config/fish/config.fish | grep fish_user_paths"
+end
+
+directory "/home/#{node.user.name}/.rbenv/plugins" do
+  action :create
+end
+
+git "ruby-build" do
+  repository "https://github.com/rbenv/ruby-build.git"
+  destination "/home/#{node.user.name}/.rbenv/plugins/ruby-build"
+end
