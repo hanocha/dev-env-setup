875c9a571d1276a33afb9701ebc4a72dd22c27ec
hanocha <shumpei.s@hanolab.net>
2022-05-05 16:15:07 +0900

    Use volta to manage node js

--- cookbooks/nodejs/default.rb
+++ cookbooks/nodejs/default.rb
@@ -1,18 +1,4 @@
-execute 'download and setup nodebrew' do
+execute 'Install volta' do
   user node.user.name
-  command 'curl -L git.io/nodebrew | perl - setup'
-  not_if "/home/#{node.user.name}/.nodebrew/current/bin/nodebrew"
-end
-
-execute "add nodebrew settings" do
-  command %|
-    echo 'set -x PATH /home/#{node.user.name}/.nodebrew/current/bin $PATH' >> /home/#{node.user.name}/.config/fish/config.fish
-|
-  not_if "grep '.nodebrew/current/bin' /home/#{node.user.name}/.config/fish/config.fish"
-end
-
-execute 'install nodejs(latest, stable)' do
-  command "/home/#{node.user.name}/.nodebrew/current/bin/nodebrew install stable; /home/hanocha/.nodebrew/current/bin/nodebrew use latest"
-  not_if "/home/#{node.user.name}/.nodebrew/current/bin/node -v"
-  user "#{node.user.name}"
+  command 'fish -c "curl https://get.volta.sh | bash"'
 end
