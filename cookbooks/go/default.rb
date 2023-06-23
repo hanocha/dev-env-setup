00c017fa66ec800e151cb885158e623ca9f237ed
hanocha <hanocha.peter@gmail.com>
2023-06-23 20:11:52 +0900

    Specify version of go in node.json

--- cookbooks/go/default.rb
+++ cookbooks/go/default.rb
@@ -1,26 +1,27 @@
 home_dir = "/home/#{node.user.name}"
 
-execute 'download go 1.17 binary' do
+execute 'download go binary' do
   user node.user.name
   cwd home_dir
-  command 'wget https://golang.org/dl/go1.17.1.linux-amd64.tar.gz -O go.tar.gz'
+  command "wget https://golang.org/dl/go#{node.goVersion}.linux-amd64.tar.gz -O go.tar.gz"
   not_if "ls #{home_dir}/go.tar.gz"
 end
 
 execute 'decompless go binary tarball' do
-  command "rm -rf /usr/local/go && tar -C /usr/local -xzf #{home_dir}/go.tar.gz"
+  command "tar -C /usr/local -xzf #{home_dir}/go.tar.gz"
+  not_if "test -e /usr/local/go"
 end
 
 execute 'add /usr/local/go/bin into PATH' do
   user node.user.name
   command "echo 'set -x PATH /usr/local/go/bin $PATH' >> #{home_dir}/.config/fish/config.fish"
-  not_if "cat #{home_dir}/.config/fish/config.fish | grep 'set -x PATH /usr/local/go/bin $PATH'"
+  not_if "grep 'set -x PATH /usr/local/go/bin $PATH' #{home_dir}/.config/fish/config.fish"
 end
 
 execute 'add ~/go/bin into PATH' do
   user node.user.name
   command "echo 'set -x PATH #{home_dir}/go/bin $PATH' >> #{home_dir}/.config/fish/config.fish"
-  not_if "cat #{home_dir}/.config/fish/config.fish | grep 'set -x PATH #{home_dir}/go/bin $PATH'"
+  not_if "grep 'set -x PATH #{home_dir}/go/bin $PATH' #{home_dir}/.config/fish/config.fish"
 end
 
 file "#{home_dir}/go.tar.gz" do
