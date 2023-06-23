c18bfefe182fc9e7ca87a108c517b8d156f92c64
hanocha <hanocha.peter@gmail.com>
2023-06-23 20:12:55 +0900

    Refactor rbenv install process

--- cookbooks/ruby/default.rb
+++ cookbooks/ruby/default.rb
@@ -2,15 +2,15 @@ git "rbenv" do
   user node.user.name
   repository "https://github.com/rbenv/rbenv.git"
   destination "/home/#{node.user.name}/.rbenv"
+  not_if "test -e /home/#{node.user.name}/.rbenv"
 end
 
 execute 'Add rbenv settings into config.fish' do
   user node.user.name
   command <<~COMMAND
-    echo 'set -Ux fish_user_paths $HOME/.rbenv/bin $fish_user_paths' >> /home/#{node.user.name}/.config/fish/config.fish
-    echo 'status --is-interactive; and rbenv init - fish | source' >> /home/#{node.user.name}/.config/fish/config.fish
+    echo 'status --is-interactive; and ~/.rbenv/bin/rbenv init - fish | source' >> /home/#{node.user.name}/.config/fish/config.fish
   COMMAND
-  not_if "cat /home/#{node.user.name}/.config/fish/config.fish | grep fish_user_paths"
+  not_if "grep '~/.rbenv/bin/rbenv init' /home/#{node.user.name}/.config/fish/config.fish"
 end
 
 directory "/home/#{node.user.name}/.rbenv/plugins" do
@@ -20,4 +20,9 @@ end
 git "ruby-build" do
   repository "https://github.com/rbenv/ruby-build.git"
   destination "/home/#{node.user.name}/.rbenv/plugins/ruby-build"
+  not_if "test -e /home/#{node.user.name}/.rbenv/plugins/ruby-build"
+end
+
+%w(autoconf patch build-essential rustc libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libgmp-dev libncurses5-dev libffi-dev libgdbm6 libgdbm-dev libdb-dev uuid-dev).each do |pkg|
+  package pkg
 end
